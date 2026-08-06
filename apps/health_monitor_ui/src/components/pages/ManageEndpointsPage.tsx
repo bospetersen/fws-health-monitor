f423cb241b446c1a22c1f3f2ca9759a9b911118d
Bo Slot Petersen <bo.s.petersen@gmail.com>
2026-08-06 15:23:34 +0200

    Updated files: 2026/08/06 15:23:34

--- apps/health_monitor_ui/src/components/pages/ManageEndpointsPage.tsx
+++ apps/health_monitor_ui/src/components/pages/ManageEndpointsPage.tsx
@@ -770,7 +770,14 @@ export default function ManageEndpointsPage() {
                         <table>
                           <thead>
                             <tr>
-                              <th style={{"width": "300px", "padding-left": "10px"}}>Name</th>
+                              <th style={{"width": "300px", "padding-left": "10px"}}>
+                                <span title="Drag rows to reorder" style={{"margin-right": "13px", "cursor": "help", "display": "inline-flex", "align-items": "center"}}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrows-expand" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10"/>
+                                  </svg>
+                                </span>
+                                Name
+                              </th>
                               <th>Description</th>
                               <th>Actions</th>
                             </tr>
@@ -807,6 +814,7 @@ export default function ManageEndpointsPage() {
                                     onMouseEnter={() => setHoveredEndpointId(endpoint._id)}
                                     onMouseLeave={() => setHoveredEndpointId(null)}
                                   >
+
                                     <td style={{"padding-left": "10px"}}>
                                       <span class={managementStyles.linkName}>{endpoint.name}</span>
                                     </td>
